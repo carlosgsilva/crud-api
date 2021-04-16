6a89b7e20331ed96a169b03f2435fdcaeb84010d
Carlos-Silva <carlosgsilva.dev@gmail.com>
2021-04-16 19:28:42 -0400

    Desenvolvimento da rota 'ListAllEmployees'

--- employee-api/src/routes/employee_routes.js
+++ employee-api/src/routes/employee_routes.js
@@ -10,7 +10,10 @@ const employeeController = require('../controller/employee_controller');
 
 // ==> Definindo as rotas do CRUD - 'Employee'
 
-// ==> Rota responsável por criar um novo 'Colaborador(a)': (POST): localhost:3000/api/employeeController
+// ==> Rota responsável por criar um novo 'Colaborador(a)': (POST): localhost:3000/api/employees
 router.post('/employees', employeeController.createEmployee);
+
+// ==> Rota responsável por listar todos os 'Colaboradores': (GET): localhost:3000/api/employees
+router.get('/employees', employeeController.listAllEmployees);
 
 module.exports = router;
