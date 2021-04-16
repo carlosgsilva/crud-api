b9462b748836c875395b29c99256e3dcb99b2201
Carlos-Silva <carlosgsilva.dev@gmail.com>
2021-04-16 15:21:43 -0400

    Desenvolvimento do arquivo 'database.js'.

---
+++ employee-api/src/config/database.js
@@ -0,0 +1,29 @@
+/**
+ * Arquivo: config/database.js
+ * Descrição: Arquivo responsável pela 'connectionStrings' (Postgres)
+ * Data: 16/04/2021
+ * Author: Carlos Silva
+ */
+
+const { Pool, Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// ==> Conexão com a Base de dados:
+const Pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+pool.on('error', (err, cliente) => {
+    console.log('Unexpected error on idle cliente', err);
+    process.exit(-1);
+})
+
+pool.on('connect', () => {
+    console.log('Base de dados conectada com sucesso!');
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+};
